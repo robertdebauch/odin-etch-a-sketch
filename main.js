d546a5b8d6b156a8b929a0943fe64db7326ee229
robert_debauch <robert.debauch@gmail.com>
2025-08-23 12:04:14 +0500

    feat: add drawing effect for the grid
    each cell can change color on hover.

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const grid = document.querySelector('#grid');
 
 // define number of cells
 
-// let num = 16;
-let num = prompt('type the number', 16);
+// let num = prompt('type the number', 16);
+let num = 16;
 let numOfCells = Math.pow(num, 2);
 
 // calculate cellSize
@@ -13,27 +13,51 @@ let numOfCells = Math.pow(num, 2);
 const gridArea = 768; // hard-code. can be parametrized in the future?
 const cellSize = gridArea / num;
 
-// var 1: for loop
+const cellArray = [];
+
+// loop to create grid
 
 for (let cell = 0; cell < numOfCells; cell++) {
-    const cell = document.createElement('div');
+    let cell = document.createElement('div');
     cell.setAttribute('class', 'cell');
     grid.appendChild(cell);
     cell.setAttribute(`style`, `width: ${cellSize}px; height: ${cellSize}px;`);
+    cellArray.push(cell);
 }
 
-console.log(numOfCells);
+// without function
+// cellArray.forEach((cell) => {
+//     cell.addEventListener('mouseover', () => {
+//         cell.classList.add('cell_filled');
+//     });
+// });
+
+// with named function
+
+cellArray.forEach((cell) => {
+    cell.addEventListener('mouseover', hoverState);
+});
+
+function hoverState(event) {
+    event.target.classList.add('cell_filled');
+}
+
 
 // information about the size of the grid
 const gridSizeInfo = document.querySelector('#current-size');
 gridSizeInfo.textContent = `current size of the grid: ${num} x ${num}`;
 
 
-
 // make button activate prompt
 
 const button = document.querySelector('button');
-function changeGrid() {
-    grid.setAttribute('style', 'background-color: red');
-}
-button.addEventListener('click', changeGrid);
+
+button.addEventListener('click', () => {
+    console.log('hello');
+})
+
+// function newGrid() {
+//     grid.setAttribute('style', 'background-color: red');
+// }
+
+// button.addEventListener('click', newGrid);

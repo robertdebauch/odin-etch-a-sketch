88e3bfd691544c61a2c8f477aa4c81b0d5184fbb
robert_debauch <robert.debauch@gmail.com>
2025-08-24 10:43:47 +0500

    feat: add prompt mechanic to define grid size

--- main.js
+++ main.js
@@ -4,8 +4,28 @@ const grid = document.querySelector('#grid');
 
 // define number of cells
 
+// let num = 16;
 // let num = prompt('type the number', 16);
-let num = 16;
+
+function defineGridSize() {
+    let i;
+
+    while (isNaN(i)) {
+        i = prompt('define the grid size: enter the number up to 100', 16);
+
+        if (!isNaN(i)) {
+            if (i > 100) {
+                i = 100;
+                return i;
+            } else {
+                return i;
+            }
+        }
+    }
+}
+
+let num = defineGridSize();
+
 let numOfCells = Math.pow(num, 2);
 
 // calculate cellSize
@@ -17,22 +37,21 @@ const cellArray = [];
 
 // loop to create grid
 
-for (let cell = 0; cell < numOfCells; cell++) {
-    let cell = document.createElement('div');
-    cell.setAttribute('class', 'cell');
-    grid.appendChild(cell);
-    cell.setAttribute(`style`, `width: ${cellSize}px; height: ${cellSize}px;`);
-    cellArray.push(cell);
+function createGrid() {
+    for (let cell = 0; cell < numOfCells; cell++) {
+        let cell = document.createElement('div');
+        cell.setAttribute('class', 'cell');
+        grid.appendChild(cell);
+        cell.setAttribute(`style`, `width: ${cellSize}px; height: ${cellSize}px;`);
+        cellArray.push(cell);
+    }
 }
 
-// without function
-// cellArray.forEach((cell) => {
-//     cell.addEventListener('mouseover', () => {
-//         cell.classList.add('cell_filled');
-//     });
-// });
+createGrid();
 
-// with named function
+// draw version: creates new grids, but it's not what i need.
+// const button = document.querySelector('button');
+// button.addEventListener('click', createGrid);
 
 cellArray.forEach((cell) => {
     cell.addEventListener('mouseover', hoverState);
@@ -42,22 +61,10 @@ function hoverState(event) {
     event.target.classList.add('cell_filled');
 }
 
-
 // information about the size of the grid
 const gridSizeInfo = document.querySelector('#current-size');
 gridSizeInfo.textContent = `current size of the grid: ${num} x ${num}`;
+if (`${num}` == 100) {
+    gridSizeInfo.textContent = `the maximum size ${num} x ${num} is reached.`;
+}
 
-
-// make button activate prompt
-
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    console.log('hello');
-})
-
-// function newGrid() {
-//     grid.setAttribute('style', 'background-color: red');
-// }
-
-// button.addEventListener('click', newGrid);

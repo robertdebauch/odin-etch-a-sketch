a43d79bf75478c39b0140d4c878158c8e308de71
robert_debauch <robert.debauch@gmail.com>
2025-08-26 10:44:38 +0500

    feat: add random color for cells

--- main.js
+++ main.js
@@ -2,10 +2,67 @@
 
 const grid = document.querySelector('#grid');
 
-// define number of cells
+let num = randomGridSize();
 
-// let num = 16;
-// let num = prompt('type the number', 16);
+function randomGridSize() {
+    let range = 100;
+    let randomNumber = Math.floor(Math.random() * range / 2) * 2;
+    if (randomNumber == 0) {
+        console.log(randomNumber);
+        return randomNumber += 1;
+    } else {
+        return randomNumber;
+    }
+}
+
+function createGrid() {
+    let gridArea = 768;
+    let cellSize = gridArea / num;
+    let numOfCells = Math.pow(num, 2);
+    let cellArray = [];
+
+    for (let cell = 0; cell < numOfCells; cell++) {
+        let cell = document.createElement('div');
+        cell.setAttribute('class', 'cell');
+        grid.appendChild(cell);
+        cell.setAttribute(`style`, `width: ${cellSize}px; height: ${cellSize}px;`);
+        cellArray.push(cell);
+    }
+
+    const randomColor = (event) => {
+        const randColor = Math.floor(Math.random() * 16777215).toString(16);
+        event.target.style.backgroundColor = "#" + randColor;
+    }
+
+    cellArray.forEach((cell) => {
+        cell.addEventListener('mouseover', randomColor);
+    });
+
+    let gridSizeInfo = document.querySelector('#current-size');
+    gridSizeInfo.textContent = `current size of the grid: ${num} x ${num}`;
+    if (`${num}` == 100) {
+        gridSizeInfo.textContent = `the maximum size ${num} x ${num} is reached.`;
+    }
+}
+
+createGrid();
+
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+
+    function deleteGrid() {
+        while (grid.firstChild) {
+            grid.firstChild.remove();
+        }
+    }
+
+    deleteGrid();
+
+    num = defineGridSize();
+    createGrid();
+});
 
 function defineGridSize() {
     let i;
@@ -37,7 +94,7 @@ function defineGridSize() {
                 const message = document.querySelector('#message');
                 message.textContent = 'Creation of the new grid was canceled by the user. Return to default values. To create a new grid click on the orange button.';
                 message.style.display = 'block';
-                
+
                 return i = 16;
             } else {
                 return i;
@@ -46,67 +103,5 @@ function defineGridSize() {
     }
 }
 
-function randomGridSize() {
-    let range = 100;
-    let randomNumber = Math.floor( Math.random() * range / 2 ) * 2;
-    return randomNumber;
-}
-
-console.log(randomGridSize());
-
-let num = defineGridSize();
-
-let numOfCells = Math.pow(num, 2);
-
-// calculate cellSize
-
-const gridArea = 768; // hard-code. can be parametrized in the future?
-const cellSize = gridArea / num;
-
-const cellArray = [];
-
-// loop to create grid
-
-function createGrid() {
-    for (let cell = 0; cell < numOfCells; cell++) {
-        let cell = document.createElement('div');
-        cell.setAttribute('class', 'cell');
-        grid.appendChild(cell);
-        cell.setAttribute(`style`, `width: ${cellSize}px; height: ${cellSize}px;`);
-        cellArray.push(cell);
-    }
-}
-
-createGrid();
-
-cellArray.forEach((cell) => {
-    cell.addEventListener('mouseover', hoverState);
-});
-
-console.log(cellArray.length);
-
-function hoverState(event) {
-    event.target.classList.add('cell_filled');
-}
-
-
-// information for user about the size of the grid
-const gridSizeInfo = document.querySelector('#current-size');
-gridSizeInfo.textContent = `current size of the grid: ${num} x ${num}`;
-if (`${num}` == 100) {
-    gridSizeInfo.textContent = `the maximum size ${num} x ${num} is reached.`;
-} 
-
-// button 
-
-const button = document.querySelector('button');
-
-// remove grid
-
-button.addEventListener('click', () => {
-    while (grid.firstChild) {
-        grid.firstChild.remove();
-    }
-});
 
 
